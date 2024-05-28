2cb235033c50a67d209c8726f975aa97b2c80ccd
Allen John <88543723+Allen9994@users.noreply.github.com>
2024-05-28 14:08:43 +0530

    Simplified level walls

--- snakes.cpp
+++ snakes.cpp
@@ -21,6 +21,7 @@ using namespace std;
 short score = 0;
 short points = 0;
 short i,j;
+char wall[] = {':','|'};
 short side = 10;
 short area = side * side;
 short speed = 400, level = 2, pace = 2, head = 0;
@@ -34,7 +35,7 @@ string bline(side+1,'"');
 condition_variable cv;
 void control(char);
 void display();
-void gameToggle(short,short);
+void gameToggle(short,bool);
 void process();
 void mainMenu();
 void fileManage(string,char);
@@ -132,11 +133,11 @@ void process()   //Brain of the program. Entire game operation happens here.
         if(head>=area)     head -= area;
         else if(head<0)    head += area;
     }
-    else if(level == 2 && (head >= area || head < 0)) gameToggle(points,420);
+    else if(level == 2 && (head >= area || head < 0)) gameToggle(points,false);
     
     static short t=0,p=0,q=0;
     static int pulse = 0;
-    if(find(trail.begin(), trail.end(), head) != trail.end()) gameToggle(points,420); 
+    if(find(trail.begin(), trail.end(), head) != trail.end()) gameToggle(points,false); 
     trail.push_back(head);
     ++pulse;
     p = trail[trail.size()-1];
@@ -157,7 +158,7 @@ void process()   //Brain of the program. Entire game operation happens here.
         map[head-side]= '|';
         map[head]= 'v';
     }else 
-    if(p/side ==(q/side)-1)
+    if(p/side == (q/side)-1)
     {
         map[head+side]= '|';
         map[head]= '^';
@@ -194,10 +195,10 @@ void process()   //Brain of the program. Entire game operation happens here.
         points += pace*level*3;
         bonus = false;
     }
-    if(pulse> 3-score) 
+    if(pulse > 3-score) 
     {
         map[trail[0]] = ' ';      //For shortening the snake length dynamically
-        if (head != insect)    trail.erase(trail.begin());
+        if (head != insect) trail.erase(trail.begin());
     }   
     display();
 }
@@ -211,7 +212,7 @@ void display()
             if(level == 2)
             {
                 if((map[(j*side)+i] == '>' && i == side-1) || (map[(j*side)+i] == '<' && i == side-1)){
-                    gameToggle(points,420);
+                    gameToggle(points,false);
                 }
             }
             else
@@ -219,12 +220,8 @@ void display()
                 if(map[(j*side)+i] == '<' && i == side-1)        head += side+1;
                 else if(map[(j*side)+i] == '>' && i == side-1)   head -= side+1;
             }
-            if(i == side-1 || i == 0)
-            {
-                if(level == 1)      cout<<":";
-                else if(level == 2) cout<<"|";
-            }
-            if(i == side-1 && j==side-1) cout<<endl<<bline;
+            if(i == side-1 || i == 0)       cout<<wall[level-1];
+            if(i == side-1 && j == side-1)  cout<<endl<<bline;
             
             switch(map[(j*side)+i])
             {
@@ -243,10 +240,10 @@ int main()
     fileManage("0",'i');
     mainMenu();
 }
-void gameToggle(short score,short k) //To exit the game when the snake bites itself
+void gameToggle(short score, bool toggle) //To exit the game when the snake bites itself
 {   
     do{
-        if(k == 420)
+        if(!toggle)
         {
             system("clear");
             fileManage(to_string(pace) + to_string(level),'s');
@@ -263,7 +260,7 @@ void mainMenu()   //Main Menu
     cout<<"\nCreated by Allen\n";
     cout<<"Press:\n1 to Play\n2 for Help\n3 for Game Settings\n4 to exit\n";
     cin>>choice;
-    if(choice == '1') gameToggle(0,0);
+    if(choice == '1') gameToggle(0,true);
     if(choice == '2')   //Instructions
     {   
         system("clear");
@@ -281,7 +278,7 @@ void mainMenu()   //Main Menu
         cin>>level;
         level = (level == 1) ? 1 : 2;
         speedSelector();
-        gameToggle(0,0);
+        gameToggle(0,true);
     }
     else exit(0);
 }
