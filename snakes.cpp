e999d86eec840d5f3196463ebd79e269ed30528b
Allen John <88543723+Allen9994@users.noreply.github.com>
2024-08-06 01:23:29 +0530

    Improved code

--- snakes.cpp
+++ snakes.cpp
@@ -18,29 +18,26 @@
 #include <time.h>
 using namespace std;
 
-short score = 0;
-short points = 0;
+short score = 0, highscore = 0, points = 0, speed = 400, level = 2, pace = 2, head = 0;
 short i,j;
-char wall[] = {':','|'};
+char value = 'd', wall[] = {':','|'};
 const short side = 10;
 const short area = side * side;
-short speed = 400, level = 2, pace = 2, head = 0;
-char value = 'd';
 short p[area-1] = {0};
 vector<int> trail(1, 0); 
 string map(area,' ');
-short highscore = 0;
 string uline(side+1,'_');
 string bline(side+1,'"');
 
 condition_variable cv;
 void control(char);
 void display();
-void gameToggle(short,bool);
+void gameToggle(bool);
 void process();
 void mainMenu();
 void fileManage(string,char);
 void speedSelector();
+void checkFileStatus();
 
 void randomize()
 {
@@ -57,19 +54,17 @@ void read_value() //inputting value from user
     tcsetattr( STDIN_FILENO, TCSANOW, &newt);
  
     char c = getchar();
-    switch(c)
-    {
-        case 'w': 
-        case 's': 
-        case 'd': 
-        case 'a': value = c;
+    if (c == 'w' || c == 's' || c == 'd' || c == 'a' || c == 't') value = c;
+    if(value == 't'){
+        system("clear");
+        gameToggle(false);
     }
 
-tcsetattr( STDIN_FILENO, TCSANOW, &oldt);
+    tcsetattr( STDIN_FILENO, TCSANOW, &oldt);
   
     cv.notify_one();
 }
-void take_input() //function to accept the value parallelly while game is proceeding.  
+void takeInput() //function to accept the value parallelly while game is proceeding.  
 {
     thread th(read_value);
     mutex mtx;
@@ -80,8 +75,7 @@ void take_input() //function to accept the value parallelly while game is procee
 }
 void control(char value) //Converts user input to the direction snake must move and stores all the movements into the array
 {   
-    switch(value)
-    {      
+    switch(value){      
         case 'd': head++;        break;
         case 's': head+=side;    break;
         case 'a': head--;        break;
@@ -92,92 +86,79 @@ void control(char value) //Converts user input to the direction snake must move 
 void process()   //Brain of the program. Entire game operation happens here. 
 {
     system("clear");
-    short i;
-    static short tr=0,insect=0,frog=101,time_=0;
-    static bool flag=false,bonus=false;
+    static short tr=0, insect=0, frog=101, time_=0;
+    static bool flag=false, bonus=false;
     if(tr != score) flag=false;
     tr=score;
-    if(!flag)
-    {
+    if(!flag){
         insect=p[score%(area/2)];
         if((insect+1)%side == 0) insect++;
         flag = true;
-        if(score%4 == 3 and !bonus)
+        if(score%4 == 3 && !bonus)
         {
             frog=p[(insect/2)%(area/2)];
             if((frog+1)%side == 0) frog++;
             bonus = true;
         }
     }
-    if(level == 1)
-    {
-        if(head>=area)     head -= area;
-        else if(head<0)    head += area;
+    if(level == 1){
+        if(head >= area)    head -= area;
+        else if(head < 0)   head += area;
     }
-    else if(level == 2 && (head >= area || head < 0)) gameToggle(points,false);
+    else if(level == 2 && (head >= area || head < 0)) gameToggle(false);
     
     static short t=0,p=0,q=0;
     static int pulse = 0;
-    if(find(trail.begin(), trail.end(), head) != trail.end()) gameToggle(points,false); 
+    if(find(trail.begin(), trail.end(), head) != trail.end()) gameToggle(false); 
     trail.push_back(head);
     ++pulse;
     p = trail[trail.size()-1];
     q = trail[trail.size()-2];
     
-    if(p == q+1)
-    {   
+    if(p == q+1){   
         map[head-1]= '=';
         map[head]= '>';
     }else 
-    if(p == q-1)
-    {
+    if(p == q-1){
         map[head+1]= '=';
         map[head]= '<';
     }else 
-    if(p/side == (q/side)+1)
-    {
+    if(p/side == (q/side)+1){
         map[head-side]= '|';
         map[head]= 'v';
     }else 
-    if(p/side == (q/side)-1)
-    {
+    if(p/side == (q/side)-1){
         map[head+side]= '|';
         map[head]= '^';
     }
-    
     map[insect]='+'; //The point which determines the score and increments the length of the snake 
     if(bonus)
     {
-        if (time_ < pulse) 
-        {
+        if (time_ < pulse){
             map[frog]='@';
             cout<<int(1.5*side)-pulse+time_<<endl;
         }
-        if (time_ == pulse-int(1.5*side)) 
-        {
+        if (time_ == pulse-int(1.5*side)){
             bonus = false; 
             map[frog] = ' ';
             frog=area+1;
         }
     }
-    else {
+    else{
         time_ = pulse;
         cout<<endl;
     }
-    if(head == insect)    
-    {
+    if(head == insect){
         cout<<"\a";
         points += pace*level;
         score++;
     }
-    if(head == frog)
-    {
+    if(head == frog){
         cout<<"\a";
         points += pace*level*3;
         bonus = false;
     }
-    if(pulse > 3-score) 
-    {
+    if(pulse > 3-score){
         map[trail[0]] = ' ';      //For shortening the snake length dynamically
         if (head != insect) trail.erase(trail.begin());
     }   
@@ -186,28 +167,23 @@ void process()   //Brain of the program. Entire game operation happens here.
 void display()
 {
     cout<<points<<endl<<uline<<endl;
-    for (j=0;j<side;j++)  //Designing the 2Dmodel : Borders not made yet
-    {   
-        for (i=0;i<side;i++)
-        {
-            if(level == 2)
-            {
+    for (j=0;j<side;j++){  //Designing the 2Dmodel : Borders not made yet   
+        for (i=0;i<side;i++){
+            if(level == 2){
                 if((map[(j*side)+i] == '>' && i == side-1) || (map[(j*side)+i] == '<' && i == side-1)){
                     cout<<endl;
                     system("clear");
-                    gameToggle(points,false);
+                    gameToggle(false);
                 }
             }
-            else
-            {
+            else{
                 if(map[(j*side)+i] == '<' && i == side-1)        head += side+1;
                 else if(map[(j*side)+i] == '>' && i == side-1)   head -= side+1;
             }
             if(i == side-1 || i == 0)       cout<<wall[level-1];
             if(i == side-1 && j == side-1)  cout<<endl<<bline;
             
-            switch(map[(j*side)+i])
-            {
+            switch(map[(j*side)+i]){
                 case '<': cout<<"◀"; break;
                 case '>': cout<<"▶"; break;
                 case '^': cout<<"▲"; break;
@@ -224,18 +200,16 @@ int main()
     randomize();
     mainMenu();
 }
-void gameToggle(short score, bool toggle) //To exit the game when the snake bites itself
-{   
-    do{
-        if(toggle) take_input();
-        else
-        {
-            fileManage(to_string(pace) + to_string(level),'s');
-            fileManage(to_string(score),'o');
-            cout<<"Game Over!\nScore:"<<score;
-            exit(0);
-        }
-    }while(toggle);
+void gameToggle(bool toggle) 
+{
+    if(toggle) takeInput();
+    else{
+        fileManage(to_string(pace) + to_string(level),'s');
+        fileManage(to_string(points),'o');
+        cout<<"Game Over!\nScore:"<<points<<"\n";
+        abort();
+    }
+    gameToggle(true);
 }
 void mainMenu()   //Main Menu
 {   
@@ -244,7 +218,7 @@ void mainMenu()   //Main Menu
     cout<<"Press:\n1 to Play\n2 for Help\n3 for Game Settings\n4 to exit\n";
     cin>>choice;
     system("clear");
-    if(choice == '1') gameToggle(0,true);
+    if(choice == '1') gameToggle(true);
     if(choice == '2')   //Instructions
     {   
         cout<<"CONTROLS\nPRESS\n w TO MOVE UPWARD\n s TO MOVE DOWNWARDS \n d TO MOVE RIGHT \n a TO MOVE LEFT";
@@ -260,9 +234,9 @@ void mainMenu()   //Main Menu
         cin>>level;
         level = (level == 1) ? 1 : 2;
         speedSelector();
-        gameToggle(0,true);
+        gameToggle(true);
     }
-    else exit(0);
+    else abort();
 }
 void fileManage(string data, char option)
 {
@@ -272,15 +246,17 @@ void fileManage(string data, char option)
         if(!fin) cout<<"Welcome to the game!"; 
         else
         {
-            string s;
-            cout<<"Welcome back to the game!\nThe highscore is ";
-            while (fin.good()) {
-                getline(fin,s);
+            string save_data;
+            while (fin.good()) getline(fin,save_data);
+            if(save_data.size() <= 2 || !all_of(save_data.begin(), save_data.end(), ::isdigit)) {
+                checkFileStatus();
+                fin.close();
+                abort();
             }
-            pace =  s[0] - '0';
-            level = s[1] - '0';
-            if (highscore < stoi(s.substr(2))) highscore = stoi(s.substr(2)) ;
-            cout<<highscore;
+            pace =  save_data[0] - '0';
+            level = save_data[1] - '0';
+            if (highscore < stoi(save_data.substr(2))) highscore = stoi(save_data.substr(2)) ;
+            cout<<"Welcome back to the game!\nThe highscore is "<<highscore;
             speedSelector();
         }
         fin.close();
@@ -290,11 +266,9 @@ void fileManage(string data, char option)
         fout<<endl<<data;
         fout.close();
     }
-    else if(option == 'o')
-    {
+    else if(option == 'o'){
         ofstream fout("snakes_data.txt", ios::app);
-        if(stoi(data) > highscore) 
-        {
+        if(stoi(data) > highscore){
             highscore = stoi(data);
             cout<<"HIGHSCORE! "<<highscore<<endl;
         }
@@ -304,10 +278,18 @@ void fileManage(string data, char option)
 }
 void speedSelector()
 {
-    switch(pace)
-    {
+    switch(pace){
         case 1: speed = 550;break;
         case 3: speed = 250;break;
         default:speed = 400;pace = 2;break;
     }
+}
+void checkFileStatus()
+{
+    cout<<"The save file is corrupted! \nKindly restart the game as the save file is reset\n";
+    ofstream fout("snakes_data.txt",ios::app);
+    fout<<endl<<"220";
+    fout.close();
+    sleep(1);
+    cout<<"Terminating..\n";
 }
